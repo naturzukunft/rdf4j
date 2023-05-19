a9d6a2dedd467dff32ab299c08931be26a6a733f
Fred Hauschel <fredrobert.hauschel@ext.opitz-consulting.com>
2023-05-19 17:43:34 +0200

    add tests

--- spring-components/spring-boot-sparql-web/src/test/java/org/eclipse/rdf4j/http/server/readonly/sparql/SparqlQueryEvaluatorDefaultTest.java
+++ spring-components/spring-boot-sparql-web/src/test/java/org/eclipse/rdf4j/http/server/readonly/sparql/SparqlQueryEvaluatorDefaultTest.java
@@ -1,14 +1,27 @@
 package org.eclipse.rdf4j.http.server.readonly.sparql;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
 import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.Literal;
 import org.eclipse.rdf4j.model.Model;
 import org.eclipse.rdf4j.model.util.ModelBuilder;
 import org.eclipse.rdf4j.model.util.Values;
+import org.eclipse.rdf4j.query.MalformedQueryException;
 import org.eclipse.rdf4j.repository.Repository;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
 import org.eclipse.rdf4j.repository.sail.SailRepository;
 import org.eclipse.rdf4j.sail.memory.MemoryStore;
 import org.junit.jupiter.api.Test;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 
 public class SparqlQueryEvaluatorDefaultTest {
 
@@ -21,26 +34,130 @@ public class SparqlQueryEvaluatorDefaultTest {
     private static final IRI TYP2 = Values.iri("http://example.com/typ2");
 
     @Test
-    public void test() {
-
+    public void queryWithoutContext() throws MalformedQueryException, IllegalStateException, IOException {
         Repository repo = new SailRepository(new MemoryStore());
-
-        Model model = new ModelBuilder()
-                .subject(Values.iri("http://example.com/user1/object1"))
-                .add(TYP1, Values.literal("testValue_user1_obj1_1"))
-                .add(TYP2, Values.literal("testValue_user1_obj1_2"))
-                .subject(Values.iri("http://example.com/user1/object2"))
-                .add(TYP1, Values.literal("testValue_user1_obj2_1"))
-                .add(TYP2, Values.literal("testValue_user1_obj2_2"))
-                .build();
-
         try (RepositoryConnection con = repo.getConnection()) {
-            con.add(model, CTX1);
+            con.add(getTestModel1(), CTX1);
+            con.add(getTestModel2(), CTX2);
             con.getStatements(null, null, null).forEach(System.out::println);
         }
 
-        // EvaluateResult evaluateResult = new EvaluateResultDefault(new ByteArrayOutputStream());
-        // SparqlQueryEvaluator sparqlQueryEvaluator = new SparqlQueryEvaluatorDefault();
+        EvaluateResult evaluateResult = new EvaluateResultDefault(new ByteArrayOutputStream());
+        SparqlQueryEvaluator sparqlQueryEvaluator = new SparqlQueryEvaluatorDefault();
 
+        String queryString = "SELECT ?subject ?predicate ?object WHERE { ?subject ?predicate ?object} ";
+        String[] namedGraphUris = new String[] {};
+
+        String defaultGraphUri = null;
+
+        sparqlQueryEvaluator.evaluate(evaluateResult, repo, queryString, null,
+                defaultGraphUri, namedGraphUris);
+
+        ArrayNode bindingArray = asArrayNode(evaluateResult);
+        assertEquals(4, bindingArray.size());
+    }
+    
+    @Test
+    public void queryWithDefaultGraphUri() throws MalformedQueryException, IllegalStateException, IOException {
+        Repository repo = new SailRepository(new MemoryStore());
+        try (RepositoryConnection con = repo.getConnection()) {
+            con.add(getTestModel1(), CTX1);
+            con.add(getTestModel2(), CTX1);
+            con.getStatements(null, null, null).forEach(System.out::println);
+        }
+
+        EvaluateResult evaluateResult = new EvaluateResultDefault(new ByteArrayOutputStream());
+        SparqlQueryEvaluator sparqlQueryEvaluator = new SparqlQueryEvaluatorDefault();
+
+        String queryString = "SELECT ?subject ?predicate ?object WHERE { ?subject ?predicate ?object} ";
+        String[] namedGraphUris = new String[] {};
+        String defaultGraphUri = CTX1.stringValue();
+
+        sparqlQueryEvaluator.evaluate(evaluateResult, repo, queryString, null,
+                defaultGraphUri, namedGraphUris);
+
+        ArrayNode bindingArray = asArrayNode(evaluateResult);
+        assertEquals(4, bindingArray.size());
+    }
+
+    @Test
+    public void queryWithDefaultGraphUriAndNamedGraphUris() throws MalformedQueryException, IllegalStateException, IOException {
+        Repository repo = new SailRepository(new MemoryStore());
+        try (RepositoryConnection con = repo.getConnection()) {
+            con.add(getTestModel1(), CTX1);
+            con.add(getTestModel2(), CTX2);
+            con.getStatements(null, null, null).forEach(System.out::println);
+        }
+
+        EvaluateResult evaluateResult = new EvaluateResultDefault(new ByteArrayOutputStream());
+        SparqlQueryEvaluator sparqlQueryEvaluator = new SparqlQueryEvaluatorDefault();
+
+        String queryString = "SELECT ?subject ?predicate ?object WHERE { ?subject ?predicate ?object} ";
+        String[] namedGraphUris = new String[] {CTX2.stringValue()};
+        String defaultGraphUri = CTX1.stringValue();
+
+        sparqlQueryEvaluator.evaluate(evaluateResult, repo, queryString, null,
+                defaultGraphUri, namedGraphUris);
+
+        ArrayNode bindingArray = asArrayNode(evaluateResult);
+        assertEquals(4, bindingArray.size());
+    }
+
+    @Test
+    public void queryWithNamedGraphUris() throws MalformedQueryException, IllegalStateException, IOException {
+        Repository repo = new SailRepository(new MemoryStore());
+        try (RepositoryConnection con = repo.getConnection()) {
+            con.add(getTestModel1(), CTX1);
+            con.add(getTestModel2(), CTX2);
+            con.getStatements(null, null, null).forEach(System.out::println);
+        }
+
+        EvaluateResult evaluateResult = new EvaluateResultDefault(new ByteArrayOutputStream());
+        SparqlQueryEvaluator sparqlQueryEvaluator = new SparqlQueryEvaluatorDefault();
+
+        String queryString = "SELECT ?subject ?predicate ?object WHERE { ?subject ?predicate ?object} ";
+        String[] namedGraphUris = new String[] {CTX1.stringValue(), CTX2.stringValue()};
+        String defaultGraphUri = null;
+
+        sparqlQueryEvaluator.evaluate(evaluateResult, repo, queryString, null,
+                defaultGraphUri, namedGraphUris);
+
+        ArrayNode bindingArray = asArrayNode(evaluateResult);
+        assertEquals(4, bindingArray.size());
+    }
+
+    private Model getTestModel1() {
+        Literal obj1_1 = Values.literal("testValue_user1_obj1_1");
+        Literal obj1_2 = Values.literal("testValue_user1_obj1_2");
+        IRI obj1 = Values.iri("http://example.com/user1/object1");
+        Model model = new ModelBuilder()
+                .subject(obj1)
+                .add(TYP1, obj1_1)
+                .add(TYP2, obj1_2)
+                .build();
+        return model;
+    }
+
+    private Model getTestModel2() {
+        Literal obj2_1 = Values.literal("testValue_user1_obj2_1");
+        Literal obj2_2 = Values.literal("testValue_user1_obj2_2");
+        IRI obj2 = Values.iri("http://example.com/user1/object2");
+
+        Model model = new ModelBuilder()
+                .subject(obj2)
+                .add(TYP1, obj2_1)
+                .add(TYP2, obj2_2)
+                .build();
+        return model;
+    }
+
+    private ArrayNode asArrayNode(EvaluateResult evaluateResult) throws IOException, JsonProcessingException, JsonMappingException {
+        ByteArrayOutputStream stream = (ByteArrayOutputStream) evaluateResult.getOutputstream();
+        String evaluateResultString = new String(stream.toByteArray());
+        System.out.println(evaluateResultString);
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readTree(evaluateResultString);
+        ArrayNode bindingArray = (ArrayNode) root.get("results").get("bindings");
+        return bindingArray;
     }
 }
